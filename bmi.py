aea9fb02894a1c4e6142a909c5c25a3bfaca05fd
fodela <particleme30@gmail.com>
2021-06-14 16:15:55 +0000

    added the increase and decrease functionality as well as the unit chosing functionality

--- bmi.py
+++ bmi.py
@@ -7,29 +7,7 @@ class ManagerScreens(ScreenManager):
 
 
 class MyHomeScreen(Screen):
-    user_height = NumericProperty(175)
-    weight = NumericProperty(75)
-    age = NumericProperty(10)
-
-    def weight_increase(self):
-        self.weight += 1
-    def weight_decrease(self):
-        self.weight -= 1
-
-    def age_increase(self):
-        self.age += 1
-    def age_decrease(self):
-        self.age -= 1
-
-    def user_height_increase(self):
-        self.user_height += 1
-    def user_height_decrease(self):
-        self.user_height -= 1
-
-    def calculate_bmi(self):
-        bmi = round((self.weight)/((self.user_height*0.01)**2),1)
-        result_popup = MDDialog(title='BMI',text=f"Your BMI is {bmi}",size_hint=[.8,.3])
-        result_popup.open()
+   pass
 
 class ResultScreen(Screen):
     pass
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import Screen,ScreenManager
 from kivy.core.window import Window
+from kivy.properties import NumericProperty, StringProperty
+from kivymd.uix.dialog import MDDialog
 
 from kivymd.uix.menu import MDDropdownMenu
 import math
@@ -18,6 +20,75 @@ Window.size = (350,600)
 
  
 class BMICalc(App,MDApp):
+    
+    height = NumericProperty(175)
+    weight = NumericProperty(75)
+    age = NumericProperty(10)
+    height_unit = StringProperty('cm')
+    weight_unit = StringProperty('kg')
+    
+    def menu_callback(self, text_item):
+        self.menu.dismiss()
+        print(text_item)
+
+    weight_items = [
+        {
+            "text": "kg",
+            "viewclass": "OneLineListItem",
+            "on_release": lambda x="kg": menu_callback(x),
+        },
+        {
+            "text": "pounds",
+            "viewclass": "OneLineListItem",
+            "on_release": lambda x="pounds": menu_callback(x),
+        },
+    ]
+    height_items =[
+        {
+            "text": "cm",
+            "viewclass": "OneLineListItem",
+            "on_release": lambda x="cm": self.menu_callback(x),
+        },
+        {
+            "text": "inches",
+            "viewclass": "OneLineListItem",
+            "on_release": lambda x="inches": self.menu_callback(x),
+        }
+    ]
+        
+    
+
+    def drop(self,option,this):
+        self.menu = MDDropdownMenu(
+            caller=this,
+            items=option,
+            width_mult=2,
+        )
+        self.menu.open()
+
+
+    
+
+    def weight_increase(self):
+        self.weight += 1
+    def weight_decrease(self):
+        self.weight -= 1
+
+    def age_increase(self):
+        self.age += 1
+    def age_decrease(self):
+        self.age -= 1
+
+    def height_increase(self):
+        self.height += 1
+    def height_decrease(self):
+        self.height -= 1
+
+    def calculate_bmi(self):
+        bmi = round((self.weight)/((self.height*0.01)**2),1)
+        result_popup = MDDialog(title='BMI',text=f"Your BMI is {bmi}",size_hint=[.8,.3])
+        result_popup.open()
+
     DEBUG=1
     
     KV_FILES = {
@@ -29,20 +100,10 @@ class BMICalc(App,MDApp):
         "ResultScreen":"bmi",
     }
     AUTORELOADER_PATHS = [(".",{"recursive":True})]
-    # def build(self):
-    #     self.sm = ScreenManager()
+    
+    
 
-    #     self.home_page = MyHomeScreen()
-    #     screen = Screen(name='home')
-    #     screen.add_widget(self.home_page)
-    #     self.sm.add_widget(screen)
-
-    #     self.result_page =ResultScreen()
-    #     screen = Screen(name='result')
-    #     screen.add_widget(self.result_page)
-    #     self.sm.add_widget(screen)
-
-    #     return self.sm
+    
     def build_app(self):
         Window.bind(on_keyboard=self._rebuild)
         self.sm = ManagerScreens()
@@ -74,6 +135,8 @@ class BMICalc(App,MDApp):
         sm.current = 'home'
     def donothing(self):
         pass
+
+    
 
 if __name__ == "__main__":
     BMICalc().run()
